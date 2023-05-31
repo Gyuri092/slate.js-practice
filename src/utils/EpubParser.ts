d0cc31bfe4c38791a4c5b4b0a72d4b2fb1e7fe99
Gyuri092 <Gyuri092@github.com>
2023-05-31 19:04:03 +0900

    feat: 불필요한 코드 삭제 및 수정, parser 구현 완료

--- src/utils/EpubParser.ts
+++ src/utils/EpubParser.ts
@@ -1,71 +1,25 @@
 import JSZip from "jszip";
-import { ICollectedData, ICollectedFile } from "../types/type";
 
-/**
- * EPUB 파일을 읽어옵니다.
- */
-export const parse = async <T extends ArrayBufferLike>(rawEPUB: T) => {
-  const zip = new JSZip();
-  await zip.loadAsync(rawEPUB);
-  return zip;
-};
-
-export const getContainerXML = async <T extends JSZip>(epubContainer: T) => {
-  const raw = epubContainer.file("META-INF/container.xml");
-  if (!raw) throw new Error("Not Matchable");
-  return await raw.async("string");
-};
-
-/**
- * 최적화를 진행합니다.
- */
-export const ignite = async (originEPUBBuffer: ArrayBufferLike) => {
-  // EPUB 파일을 해석합니다.
-  const epubContainer = await parse(originEPUBBuffer);
-
-  // 필요한 데이터를 수집합니다.
-  const data = await collect(epubContainer);
-
-  return data;
-};
-
-/**
- * 필요한 데이터들을 획득합니다.
- */
-export const collect = async (
-  epubContainer: JSZip
-): Promise<ICollectedData> => {
-  // 필요한 데이터를 수집합니다.
-  const containerXML = await getContainerXML(epubContainer);
-
-  // 수집된 데이터들
-  const files: ICollectedFile[] = [];
-
-  for (const file of Object.values(epubContainer.files)) {
-    // 폴더 자체는 수정하지 않습니다.
-    if (file.dir) continue;
-
-    // 경로를 파싱합니다.
-    const filePaths = file.name.split("/");
-    const fileFullName = filePaths[filePaths.length - 1];
-    const [fileName, fileExt] = fileFullName.split(".");
-
-    // 오직 OEBPS 폴더 내 파일만 분석합니다.
-    // Open eBook Publication Structure (OEBPS)
-    if (filePaths[0] !== "OEBPS") continue;
-
-    // root 폴더와 파일명을 경로에서 제외합니다.
-    filePaths.shift();
-    filePaths.pop();
-
-    files.push({
-      file,
-      fileExt,
-      originName: file.name,
-      fileName,
-      filePaths,
-    });
+export const parseContentOpf = async (zippedEpub: JSZip) => {
+  const contentOpf = Object.values(zippedEpub.files).find(({ name }) =>
+    name.endsWith(".opf")
+  );
+  if (!contentOpf) {
+    throw new Error("Invalid epub file");
   }
-
-  return { containerXML, files };
+  const parsedContentOpf = new DOMParser().parseFromString(
+    await contentOpf.async("string"),
+    "text/xml"
+  );
+  const spine = parsedContentOpf.getElementsByTagName("spine").item(0);
+  if (!spine) {
+    throw new Error("Invalid epub file");
+  }
+  const order = Array.from(spine.getElementsByTagName("itemref"))
+    .map((ref) => ref.getAttribute("idref"))
+    .filter((idref): idref is string => !!idref);
+  return {
+    document: parsedContentOpf,
+    order,
+  };
 };
--- src/utils/LoadEpub.ts
+++ src/utils/LoadEpub.ts
@@ -1,8 +1,8 @@
 import { BaseEditor } from "slate";
 import { ReactEditor } from "slate-react";
 import { insertFile } from "./InsertSlate";
-import { ignite, parse } from "./EpubParser";
 import { setStateFunctionType } from "../types/type";
+import JSZip from "jszip";
 
 export const LoadEpub = async (
   event: React.ChangeEvent<HTMLInputElement>,
@@ -13,31 +13,93 @@ export const LoadEpub = async (
   if (!files) {
     return;
   }
-  const [file] = files;
-  if (!file) {
+
+  const [epubFile] = files;
+  if (!epubFile || !epubFile.name.endsWith(".epub")) {
     return;
   }
 
-  const epubFile = await file.arrayBuffer();
-  const data = await ignite(epubFile);
-  const zip = await parse(epubFile);
+  // load epub contents
+  const instance = new JSZip();
+  const zippedEpub = await instance.loadAsync(epubFile);
 
-  const htmlContents = await Promise.all(
-    data.files
-      .filter((item) => item.fileExt === "xml" || item.fileExt === "html")
-      .map(async (item) => {
-        const htmlFile = zip.file(item.originName);
-        const htmlXml = await htmlFile?.async("string");
-        if (!htmlXml) {
-          return;
-        }
-        const htmlDoc = new DOMParser().parseFromString(
-          htmlXml,
-          "application/xml"
-        );
-        return htmlDoc.documentElement.textContent;
-      })
+  // get metadata from container.xml
+  const containerXml = await zippedEpub.files["META-INF/container.xml"];
+  if (!containerXml) {
+    throw new Error("Invalid epub file");
+  }
+  const parsedContainerXml = new DOMParser().parseFromString(
+    await containerXml.async("string"),
+    "text/xml"
   );
-  const importText = htmlContents.toString();
-  insertFile(editor, importText, setStateFunction);
+  const rootfile = parsedContainerXml.getElementsByTagName("rootfile").item(0);
+  if (!rootfile) {
+    throw new Error("Invalid epub file");
+  }
+  const opfPath = rootfile.getAttribute("full-path");
+  if (!opfPath) {
+    throw new Error("Invalid epub file");
+  }
+  const [firstSegment, lastSegment] = opfPath.split("/");
+  const rootDir = lastSegment ? firstSegment : "";
+
+  // get content order from content opf
+  const contentOpf = zippedEpub.files[opfPath];
+  if (!contentOpf) {
+    throw new Error("Invalid epub file");
+  }
+  const parsedContentOpf = new DOMParser().parseFromString(
+    await contentOpf.async("string"),
+    "text/xml"
+  );
+  const manifest = parsedContentOpf.getElementsByTagName("manifest").item(0);
+  const spine = parsedContentOpf.getElementsByTagName("spine").item(0);
+  if (!manifest || !spine) {
+    throw new Error("Invalid epub file");
+  }
+  /* image 파일 파싱되지 않도록 필터 */
+  const items = Array.from(manifest.getElementsByTagName("item")).filter(
+    (item) => item.getAttribute("media-type") !== "image/jpeg"
+  );
+  const contentOrder = Array.from(spine.getElementsByTagName("itemref"))
+    .filter((itemref) => itemref.getAttribute("linear") !== "no") // cover image 글자 출력되지 않도록 필터
+    .map((ref) => ref.getAttribute("idref"))
+    .map((idref) => {
+      const found = items.find((item) => item.getAttribute("id") === idref);
+      if (!found) {
+        throw new Error("Invalid epub file");
+      }
+      return found.getAttribute("href");
+    })
+    .filter((href): href is string => !!href)
+    .map((href) => (rootDir ? `${rootDir}/${href}` : href));
+  // sort files by content order
+  const rawHTMLContents = await Promise.all(
+    contentOrder.map((href) => {
+      const { [href]: file } = zippedEpub.files;
+      if (!file) {
+        throw new Error("Invalid epub file");
+      }
+      return file.async("string");
+    })
+  );
+  const parser = new DOMParser();
+  const htmlTextContents = rawHTMLContents
+    .filter((contents): contents is string => !!contents)
+    .map((contents) => {
+      const htmlDoc = parser.parseFromString(contents, "application/xhtml+xml");
+      return htmlDoc.getElementsByTagName("parsererror").length > 0
+        ? ""
+        : htmlDoc.documentElement.textContent
+            ?.split("\n")
+            .map((text) => text.trim())
+            .filter((text) => !!text)
+            .join("\n");
+    })
+    .filter((text): text is string => {
+      return !!text;
+    })
+    .join("\n");
+
+  insertFile(editor, htmlTextContents, setStateFunction);
 };
